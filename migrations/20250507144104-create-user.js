6e25ea3d301b64d1abca503d96ec898fc2d9bfc8
Caio Claudino <caiovenancio06@gmail.com>
2025-05-07 12:41:09 -0300

    feat: add sequelize and migrate for user table

---
+++ migrations/20250507144104-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
